5d3e511631b1faea2222b6a15c09b5512a7f370d
Gonzalo <gonzalos.camara@gmail.com>
2024-09-18 13:43:32 +0200

    carpeta clase

---
+++ Ejercicio_multiplicar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Ejercicio multiplicar</title>
+</head>
+<body>
+<?php
+$const = 7;
+for ($i=0; $i < ; $i++) { 
+    for ($j=0; $j < ; $j++) { 
+        print "$j x $i = $jx$i /br";
+    }
+
+}
+
+?>   
+</body>
+</html>
